a9969ef8305d8c6ebd0082d25288f3b93e1aaf75
Omri Cohen <omriochen1511@gmail.com>
2024-03-29 15:19:58 +0300

    Add new datasets, fix broken data, upload 4 datas show 4 Lines

--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
 /* global d3, _ */
 
 (function() {
-  var margin = {top: 30, right: 20, bottom: 100, left: 50},
+  var margin = {top: 50, right: 20, bottom: 100, left: 50},
     margin2  = {top: 210, right: 20, bottom: 20, left: 50},
     width    = 764 - margin.left - margin.right,
     height   = 283 - margin.top - margin.bottom,
@@ -31,7 +31,7 @@
     .interpolate('monotone')
     .x(function(d) { return x(d.date); })
     .y(function(d) { return y(d.average); });
-
+  
   var area2 = d3.svg.area()
     .interpolate('monotone')
     .x(function(d) { return x2(d.date); })
@@ -84,196 +84,204 @@
     .attr('class', 'chart__range-selection')
     .attr('transform', 'translate(110, 0)');
 
-  d3.csv('./data/aapl.csv', type, function(err, data) {
-    var brush = d3.svg.brush()
-      .x(x2)
-      .on('brush', brushed);
+  var colors = ['blue', 'orange', 'red', 'black'];
+  var filePaths = ['./data/STOCKHISTORY_QQQ.csv', './data/STOCKHISTORY_SPY.csv', './data/STOCKHISTORY_ACWI.csv', './data/STOCKHISTORY_SOXX.csv'];
+  filePaths.forEach(function(filePath, counter) {
+    d3.csv(filePath, type, function(err, data) {
+      var brush = d3.svg.brush()
+        .x(x2)
+        .on('brush', brushed);
+  
+      console.log(data);
+      var xRange = d3.extent(data.map(function(d) { return d.date; }));
+  
+      x.domain(xRange);
+      y.domain(d3.extent(data.map(function(d) { return d.price; })));
+      y3.domain(d3.extent(data.map(function(d) { return d.price; })));
+      x2.domain(x.domain());
+      y2.domain(y.domain());
+  
+      var min = d3.min(data.map(function(d) { return d.price; }));
+      var max = d3.max(data.map(function(d) { return d.price; }));
+  
+      var range = legend.append('text')
+        .text(legendFormat(new Date(xRange[0])) + ' - ' + legendFormat(new Date(xRange[1])))
+        .style('text-anchor', 'end')
+        .attr('transform', 'translate(' + width + ', 0)');
+  
+      focus.append('g')
+          .attr('class', 'y chart__grid')
+          .call(make_y_axis()
+          .tickSize(-width, 0, 0)
+          .tickFormat(''));
 
-    var xRange = d3.extent(data.map(function(d) { return d.date; }));
-
-    x.domain(xRange);
-    y.domain(d3.extent(data.map(function(d) { return d.price; })));
-    y3.domain(d3.extent(data.map(function(d) { return d.price; })));
-    x2.domain(x.domain());
-    y2.domain(y.domain());
-
-    var min = d3.min(data.map(function(d) { return d.price; }));
-    var max = d3.max(data.map(function(d) { return d.price; }));
-
-    var range = legend.append('text')
-      .text(legendFormat(new Date(xRange[0])) + ' - ' + legendFormat(new Date(xRange[1])))
-      .style('text-anchor', 'end')
-      .attr('transform', 'translate(' + width + ', 0)');
-
-    focus.append('g')
-        .attr('class', 'y chart__grid')
-        .call(make_y_axis()
-        .tickSize(-width, 0, 0)
-        .tickFormat(''));
-
-    var averageChart = focus.append('path')
-        .datum(data)
-        .attr('class', 'chart__line chart__average--focus line')
-        .attr('d', avgLine);
-
-    var priceChart = focus.append('path')
-        .datum(data)
-        .attr('class', 'chart__line chart__price--focus line')
-        .attr('d', priceLine);
-
-    focus.append('g')
-        .attr('class', 'x axis')
-        .attr('transform', 'translate(0 ,' + height + ')')
-        .call(xAxis);
-
-    focus.append('g')
-        .attr('class', 'y axis')
-        .attr('transform', 'translate(12, 0)')
-        .call(yAxis);
-
-    var focusGraph = barsGroup.selectAll('rect')
-        .data(data)
-      .enter().append('rect')
-        .attr('class', 'chart__bars')
-        .attr('x', function(d, i) { return x(d.date); })
-        .attr('y', function(d) { return 155 - y3(d.price); })
-        .attr('width', 1)
-        .attr('height', function(d) { return y3(d.price); });
-
-    var helper = focus.append('g')
-      .attr('class', 'chart__helper')
-      .style('text-anchor', 'end')
-      .attr('transform', 'translate(' + width + ', 0)');
-
-    var helperText = helper.append('text')
-
-    var priceTooltip = focus.append('g')
-      .attr('class', 'chart__tooltip--price')
-      .append('circle')
-      .style('display', 'none')
-      .attr('r', 2.5);
-
-    var averageTooltip = focus.append('g')
-      .attr('class', 'chart__tooltip--average')
-      .append('circle')
-      .style('display', 'none')
-      .attr('r', 2.5);
-
-    var mouseArea = svg.append('g')
-      .attr('class', 'chart__mouse')
-      .append('rect')
-      .attr('class', 'chart__overlay')
-      .attr('width', width)
-      .attr('height', height)
-      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
-      .on('mouseover', function() {
-        helper.style('display', null);
-        priceTooltip.style('display', null);
-        averageTooltip.style('display', null);
-      })
-      .on('mouseout', function() {
-        helper.style('display', 'none');
-        priceTooltip.style('display', 'none');
-        averageTooltip.style('display', 'none');
-      })
-      .on('mousemove', mousemove);
-
-    context.append('path')
-        .datum(data)
-        .attr('class', 'chart__area area')
-        .attr('d', area2);
-
-    context.append('g')
-        .attr('class', 'x axis chart__axis--context')
-        .attr('y', 0)
-        .attr('transform', 'translate(0,' + (height2 - 22) + ')')
-        .call(xAxis2);
-
-    context.append('g')
-        .attr('class', 'x brush')
-        .call(brush)
-      .selectAll('rect')
-        .attr('y', -6)
-        .attr('height', height2 + 7);
-
-    function mousemove() {
-      var x0 = x.invert(d3.mouse(this)[0]);
-      var i = bisectDate(data, x0, 1);
-      var d0 = data[i - 1];
-      var d1 = data[i];
-      var d = x0 - d0.date > d1.date - x0 ? d1 : d0;
-      helperText.text(legendFormat(new Date(d.date)) + ' - Price: ' + d.price + ' Avg: ' + d.average);
-      priceTooltip.attr('transform', 'translate(' + x(d.date) + ',' + y(d.price) + ')');
-      averageTooltip.attr('transform', 'translate(' + x(d.date) + ',' + y(d.average) + ')');
-    }
-
-    function brushed() {
-      var ext = brush.extent();
-      if (!brush.empty()) {
-        x.domain(brush.empty() ? x2.domain() : brush.extent());
-        y.domain([
-          d3.min(data.map(function(d) { return (d.date >= ext[0] && d.date <= ext[1]) ? d.price : max; })),
-          d3.max(data.map(function(d) { return (d.date >= ext[0] && d.date <= ext[1]) ? d.price : min; }))
-        ]);
-        range.text(legendFormat(new Date(ext[0])) + ' - ' + legendFormat(new Date(ext[1])))
-        focusGraph.attr('x', function(d, i) { return x(d.date); });
-
-        var days = Math.ceil((ext[1] - ext[0]) / (24 * 3600 * 1000))
-        focusGraph.attr('width', (40 > days) ? (40 - days) * 5 / 6 : 5)
+      var averageChart = focus.append('path')
+          .datum(data)
+          .attr('class', 'chart__line chart__average--focus line')
+          .attr('d', avgLine);
+      var priceChart = focus.append('path')
+          .datum(data)
+          .attr('class', 'chart__line chart__price--focus line')
+          .attr('d', priceLine)
+          .style("stroke", colors[counter]); // Set the line color to blue
+  
+      focus.append('g')
+          .attr('class', 'x axis')
+          .attr('transform', 'translate(0 ,' + height + ')')
+          .call(xAxis);
+  
+      focus.append('g')
+          .attr('class', 'y axis')
+          .attr('transform', 'translate(12, 0)')
+          .call(yAxis);
+  
+      var focusGraph = barsGroup.selectAll('rect')
+          .data(data)
+        .enter().append('rect')
+          .attr('class', 'chart__bars')
+          .attr('x', function(d, i) { return x(d.date); })
+          .attr('y', function(d) { return 155 - y3(d.price); })
+          .attr('width', 1)
+          .attr('height', function(d) { return y3(d.price); });
+  
+      var helper = focus.append('g')
+        .attr('class', 'chart__helper')
+        .style('text-anchor', 'end')
+        .attr('transform', 'translate(' + width + ', 0)');
+  
+      var helperText = helper.append('text')
+  
+      var priceTooltip = focus.append('g')
+        .attr('class', 'chart__tooltip--price')
+        .append('circle')
+        .style('display', 'none')
+        .attr('r', 2.5);
+  
+      var averageTooltip = focus.append('g')
+        .attr('class', 'chart__tooltip--average')
+        .append('circle')
+        .style('display', 'none')
+        .attr('r', 2.5);
+  
+      var mouseArea = svg.append('g')
+        .attr('class', 'chart__mouse')
+        .append('rect')
+        .attr('class', 'chart__overlay')
+        .attr('width', width)
+        .attr('height', height)
+        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+        .on('mouseover', function() {
+          helper.style('display', null);
+          priceTooltip.style('display', null);
+          averageTooltip.style('display', null);
+        })
+        .on('mouseout', function() {
+          helper.style('display', 'none');
+          priceTooltip.style('display', 'none');
+          averageTooltip.style('display', 'none');
+        })
+        .on('mousemove', mousemove);
+  
+      context.append('path')
+          .datum(data)
+          .attr('class', 'chart__area area')
+          .attr('d', area2);
+  
+      context.append('g')
+          .attr('class', 'x axis chart__axis--context')
+          .attr('y', 0)
+          .attr('transform', 'translate(0,' + (height2 - 22) + ')')
+          .call(xAxis2);
+  
+      context.append('g')
+          .attr('class', 'x brush')
+          .call(brush)
+        .selectAll('rect')
+          .attr('y', -6)
+          .attr('height', height2 + 7);
+  
+      function mousemove() {
+        var x0 = x.invert(d3.mouse(this)[0]);
+        var i = bisectDate(data, x0, 1);
+        var d0 = data[i - 1];
+        var d1 = data[i];
+        var d = x0 - d0.date > d1.date - x0 ? d1 : d0;
+        helperText.text(legendFormat(new Date(d.date)) + ' - Price: ' + d.price + ' Avg: ' + d.average);
+        priceTooltip.attr('transform', 'translate(' + x(d.date) + ',' + y(d.price) + ')');
+        averageTooltip.attr('transform', 'translate(' + x(d.date) + ',' + y(d.average) + ')');
       }
-
-      priceChart.attr('d', priceLine);
-      averageChart.attr('d', avgLine);
-      focus.select('.x.axis').call(xAxis);
-      focus.select('.y.axis').call(yAxis);
-    }
-
-    var dateRange = ['1w', '1m', '3m', '6m', '1y', '5y']
-    for (var i = 0, l = dateRange.length; i < l; i ++) {
-      var v = dateRange[i];
-      rangeSelection
-        .append('text')
-        .attr('class', 'chart__range-selection')
-        .text(v)
-        .attr('transform', 'translate(' + (18 * i) + ', 0)')
-        .on('click', function(d) { focusOnRange(this.textContent); });
-    }
-
-    function focusOnRange(range) {
-      var today = new Date(data[data.length - 1].date)
-      var ext = new Date(data[data.length - 1].date)
-
-      if (range === '1m')
-        ext.setMonth(ext.getMonth() - 1)
-
-      if (range === '1w')
-        ext.setDate(ext.getDate() - 7)
-
-      if (range === '3m')
-        ext.setMonth(ext.getMonth() - 3)
-
-      if (range === '6m')
-        ext.setMonth(ext.getMonth() - 6)
-
-      if (range === '1y')
-        ext.setFullYear(ext.getFullYear() - 1)
-
-      if (range === '5y')
-        ext.setFullYear(ext.getFullYear() - 5)
-
-      brush.extent([ext, today])
-      brushed()
-      context.select('g.x.brush').call(brush.extent([ext, today]))
-    }
-
-  })// end Data
+  
+      function brushed() {
+        var ext = brush.extent();
+        if (!brush.empty()) {
+          x.domain(brush.empty() ? x2.domain() : brush.extent());
+          y.domain([
+            d3.min(data.map(function(d) { return (d.date >= ext[0] && d.date <= ext[1]) ? d.price : max; })),
+            d3.max(data.map(function(d) { return (d.date >= ext[0] && d.date <= ext[1]) ? d.price : min; }))
+          ]);
+          range.text(legendFormat(new Date(ext[0])) + ' - ' + legendFormat(new Date(ext[1])))
+          focusGraph.attr('x', function(d, i) { return x(d.date); });
+  
+          var days = Math.ceil((ext[1] - ext[0]) / (24 * 3600 * 1000))
+          focusGraph.attr('width', (40 > days) ? (40 - days) * 5 / 6 : 5)
+        }
+  
+        priceChart.attr('d', priceLine);
+        averageChart.attr('d', avgLine);
+        focus.select('.x.axis').call(xAxis);
+        focus.select('.y.axis').call(yAxis);
+      }
+  
+      var dateRange = ['1w', '1m', '3m', '6m', '1y', '5y']
+      for (var i = 0, l = dateRange.length; i < l; i ++) {
+        var v = dateRange[i];
+        rangeSelection
+          .append('text')
+          .attr('class', 'chart__range-selection')
+          .text(v)
+          .attr('transform', 'translate(' + (18 * i) + ', 0)')
+          .on('click', function(d) { focusOnRange(this.textContent); });
+      }
+  
+      function focusOnRange(range) {
+        var today = new Date(data[data.length - 1].date)
+        var ext = new Date(data[data.length - 1].date)
+  
+        if (range === '1m')
+          ext.setMonth(ext.getMonth() - 1)
+  
+        if (range === '1w')
+          ext.setDate(ext.getDate() - 7)
+  
+        if (range === '3m')
+          ext.setMonth(ext.getMonth() - 3)
+  
+        if (range === '6m')
+          ext.setMonth(ext.getMonth() - 6)
+  
+        if (range === '1y')
+          ext.setFullYear(ext.getFullYear() - 1)
+  
+        if (range === '5y')
+          ext.setFullYear(ext.getFullYear() - 5)
+  
+        brush.extent([ext, today])
+        brushed()
+        context.select('g.x.brush').call(brush.extent([ext, today]))
+      }
+    })// end Data
+  });
 
   function type(d) {
+    // Remove commas and other non-numeric characters except decimal point
+    var cleanVolume = d.Volume.replace(/,/g, ''); // Removes commas
+    var cleanPrice = d.Close.replace(/[^\d.]/g, ''); // Removes everything except digits and decimal point
+  
     return {
-      date    : parseDate(d.Date),
-      price   : +d.Close,
-      average : +d.Average,
-      volume : +d.Volume,
-    }
+      date: parseDate(d.Date),
+      price: +cleanPrice, // Convert cleaned price string to number
+      average: +d.Average, // Assuming 'Average' is already in a correct numeric format
+      volume: +cleanVolume, // Convert cleaned volume string to number
+    };
   }
 }());
